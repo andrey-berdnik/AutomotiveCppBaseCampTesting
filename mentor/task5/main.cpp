7c8227f08dfc42db2f95569f940834b7256bdd42
Andrey Berdnik <andrey.berdnik@gmail.com>
2021-12-28 15:05:02 +0200

    mentor task 5

--- mentor/task5/main.cpp
+++ mentor/task5/main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
-int main(int argc, char** argv)
+#include <csignal>
+#include <unistd.h>
+#include <unistd.h>
+
+
+using namespace std;
+
+void signalHandler(int signum)
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	std::cout << "SIGHUP signal received received.\n";
+	exit(signum);
+}
+
+int main(void)
+{
+	signal(SIGHUP, signalHandler);
+
+	pid_t pid;
+
+	printf("before fork\n");
+
+	if ((pid = fork()) < 0)
+	{
+		cout <<  "fork error";
+	}
+	else if (pid == 0)
+	{ /* child */
+		while (true)
+		{
+			cout << "\n------------------------------------------------"
+			<< "\nchild:"
+			<< "\nprocess ID of calling process\t=\t" << (long)getpid()
+			<< "\nparent process ID of calling process\t=\t" << (long)getppid()
+			<< "\nreal user ID of calling process\t=\t" << (long)getuid()
+			<< "\neffective user ID of calling process\t=\t" << (long)geteuid()
+			<< "\nreal group ID of calling process\t=\t" << (long)getgid()
+			<< "\neffective group ID of calling process\t=\t" << (long)getegid()
+			<< "\nprocess ID of calling process\t=\t" << (long)getpid()
+			<< "\n------------------------------------------------"
+			<< endl;
+			sleep(2);
+		}
+	}
+	else
+	{ /* parent */
+		while (true)
+		{
+			cout << "\n------------------------------------------------"
+			<< "\nparent:"
+			<< "\nprocess ID of calling process\t=\t" << (long)getpid()
+			<< "\nparent process ID of calling process\t=\t" << (long)getppid()
+			<< "\nreal user ID of calling process\t=\t" << (long)getuid()
+			<< "\neffective user ID of calling process\t=\t" << (long)geteuid()
+			<< "\nreal group ID of calling process\t=\t" << (long)getgid()
+			<< "\neffective group ID of calling process\t=\t" << (long)getegid()
+			<< "\nprocess ID of calling process\t=\t" << (long)getpid()
+			<< "\n------------------------------------------------"
+			<< endl;
+			sleep(2);
+		}
+	}
+
+	exit(0);
 }
